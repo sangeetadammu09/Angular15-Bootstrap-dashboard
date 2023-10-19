15c7bdb930c2eff9e92976323656fbaedc38fe69
sangeetadammo09 <sangeetadammu12@gmail.com>
2023-10-19 13:17:00 +0530

    errors fixed

--- src/app/layout/components/dashboard/dashboard.component.ts
+++ src/app/layout/components/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { routerTransition } from '../../router.animations';
+
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss'],
-    animations: [routerTransition()]
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
     public alerts: Array<any> = [];
